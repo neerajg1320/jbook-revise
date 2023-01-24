106839e381a13555c4f7aad0e4f16dc6f931d2d1
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-01-24 20:09:39 +0530

    Initial commit

---
+++ project.cmd
@@ -0,0 +1,6 @@
+npx create-react-app jbook-revise --template typescript
+npm install --save-exact esbuild-wasm@0.8.27
+
+# delete all files in src folder
+
+touch index.tsx
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {useState} from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const App = () => {
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+
+    const onSubmit = () => {
+        // console.log(input);
+    }
+
+    return (
+        <div>
+            <textarea
+                value={input}
+                onChange={e => setInput(e.target.value)}
+            />
+            <div>
+                <button onClick={onSubmit}>Submit</button>
+            </div>
+
+            <pre>{code}</pre>
+        </div>
+    );
+}
+
+const rootElement = document.getElementById("root") as HTMLElement;
+const root = createRoot(rootElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
