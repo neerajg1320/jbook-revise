9a9101cc959e278b30fc33115e9dca0d7609e07e
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-01-25 22:56:13 +0530

    Reversed the changes for refactorization

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-
+import * as esbuild from 'esbuild-wasm';
 import React, {useEffect, useState, useRef} from "react";
 import { createRoot } from "react-dom/client";
 import {debug} from "./global/config";
-
+import {unpkgPathPlugin} from "./plugins/unpkg-path-plugin";
+import {fetchPlugin} from "./plugins/fetch-plugin";
 import CodeEditor from "./components/editor/code-editor";
-import Preview from "./components/preview";
-import bundle from "./bundler";
 
 
 const defaultCode = `\
@@ -52,13 +51,33 @@ const showCodePreview = false;
 const eagerBundling = false;
 
 const App = () => {
-    const [input, setInput] = useState(defaultCode);
+    const serviceRef = useRef<any>();
+    const iframeRef = useRef<any>();
+    const [input, setInput] = useState(defaultErrorCode);
     const [code, setCode] = useState('');
+
+    const startService = async () => {
+        serviceRef.current =  await esbuild.startService({
+            worker: true,
+            // wasmURL: '/esbuild.wasm' // picks esbuild.wasm placed in public folder
+            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
+        });
+    };
+
+    useEffect(() => {
+        startService()
+    }, []);
 
     const onSubmit = async (value: string|null) => {
         if (debug) {
             console.log(input);
         }
+
+        if (!serviceRef.current) {
+            return;
+        }
+
+        iframeRef.current.srcdoc = html;
 
         let startCode;
         if (eagerBundling) {
@@ -66,15 +85,41 @@ const App = () => {
         } else {
             startCode = input;
         }
+        // Disabled when we switched over to build instead of transform
+        //
+        // console.log(serviceRef.current);
+        // const result = await serviceRef.current.transform(input, {
+        //     loader: 'jsx',
+        //     target: 'es2015'
+        // });
 
         // The build call using esbuild which creates a bundle
-        const output = await bundle(startCode);
-        setCode(output);
+        const result = await serviceRef.current.build({
+            entryPoints: ['index.jsx'],
+            bundle: true,
+            write: false,
+            // TBVE: Check if we can create an inmemory file and pass path to it
+            plugins: [
+                unpkgPathPlugin(),
+                fetchPlugin(startCode)
+            ],
+            define: {
+                'process.env.NODE_ENV': '"production"',
+                global: 'window'
+            }
+        })
+
+        if (debug) {
+            console.log(`result:`, result.outputFiles[0].text);
+        }
+
+        setCode(result.outputFiles[0].text);
+        iframeRef.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
 
         if (evalInMain) {
             try {
                 // The eval here causes the generated js and css to be applied on current application
-                eval(output);
+                eval(result.outputFiles[0].text);
             } catch (error) {
                 alert(error);
             }
@@ -83,13 +128,49 @@ const App = () => {
 
     const fontSize = "1.2em";
 
+    const htmlSnippetUnsed = `
+    <script>
+        ${code}
+    </script>
+    `
+    const html = `\
+    <html>
+    <head></head>
+    <body>
+        <div id="root"></div>
+        <script>
+            window.addEventListener('message', (event) => {
+              try {
+                eval(event.data);  
+              } catch (err) {
+                const root = document.querySelector('#root');
+                root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+                console.error(err);
+              }
+            }, false);
+        </script>
+    </body>
+    </html>
+    `;
+
     return (
         <div>
             <CodeEditor
-                initialValue={input}
+                initialValue="const a = 1;"
                 onChange={setInput}
             />
-
+            <textarea
+                value={input}
+                onChange={e => {
+                    setInput(e.target.value);
+                    if (eagerBundling) {
+                        onSubmit(e.target.value);
+                    }
+                }}
+                cols={80}
+                rows={10}
+                style={{fontSize}}
+            />
             <div>
                 {!eagerBundling &&
                     <button
@@ -101,7 +182,12 @@ const App = () => {
                 }
             </div>
 
-            <Preview code={code} />
+            <iframe
+                ref={iframeRef}
+                title="userCode"
+                sandbox="allow-scripts"
+                srcDoc={html}
+            />
 
             {showCodePreview && <pre style={{fontSize}}>{code}</pre>}
         </div>
