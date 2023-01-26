a35521edc5540ed56b3c70ae925612b81d029c7d
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-01-26 16:55:15 +0530

    Fixed the AddCell component which is a divider line

--- src/components/cell-list/cell-list.tsx
+++ src/components/cell-list/cell-list.tsx
@@ -1,5 +1,7 @@
+import {Fragment} from "react";
 import {useTypedSelector} from "../../hooks/use-typed-selector";
 import CellListItem from "./cell-list-item/cell-list-item";
+import AddCell from "./add-cell/add-cell";
 
 const CellList: React.FC = () => {
     const cells = useTypedSelector(({cells:{data, order}}) => {
@@ -8,10 +10,20 @@ const CellList: React.FC = () => {
         })
     });
 
-    const renderedCells = cells.map(cell => <CellListItem key={cell.id} cell={cell}/>)
+    const renderedCells = cells.map(cell => (
+        <Fragment key={cell.id}>
+            <CellListItem cell={cell}/>
+            <AddCell prevCellId={cell.id} forceVisible={false} />
+        </Fragment>
+    ))
 
     return (
-        <div>{renderedCells}</div>
+        <div>
+            <AddCell prevCellId={null} forceVisible={cells.length === 0} />
+            <div>
+                {renderedCells}
+            </div>
+        </div>
     );
 }
 
--- src/components/code-cell/code-cell.tsx
+++ src/components/code-cell/code-cell.tsx
@@ -32,7 +32,7 @@ const CodeCell: React.FC<CodeCellProps> = ({cell}) => {
     return (
         <Resizable direction="vertical">
             <div style={{
-                height: '100%',
+                height: 'calc(100% - 10px)',
                 display: "flex", flexDirection:"row"
             }}>
                 <Resizable direction="horizontal">
