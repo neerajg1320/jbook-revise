2478ef39856a5ddb0042bde2821ad37b07881d81
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-01-27 11:40:59 +0530

    Added support for path and file

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,9 +1,21 @@
+import path from "path";
 import {Command} from "commander";
+import {serve} from "local-api";
+
+export const defaultFilename = 'notebook.js';
 
 export const serveCommand = new Command()
     .command('serve [filename]')
     .description('Open a file for editing')
     .option('-p, --port <number>', 'server port', '4005')
-    .action(() => {
-        console.log('Getting ready to serve a file');
+    .action((filename = defaultFilename, options: {port: string}) => {
+        console.log(
+            'folder:',
+            path.join(process.cwd(), path.dirname(filename))
+        );
+        console.log(
+            'file',
+            path.basename(filename)
+        );
+        serve(parseInt(options.port), filename, '/');
     })
